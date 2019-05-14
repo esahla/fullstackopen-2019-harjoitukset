5628ca272e9672d013fb1c73c00041c3e9fd2d85
Esa Sahla <esa.sahla@nokia.com>
2019-05-14 11:22:19 +0300

    Harkka osa 4, logger lisätty

--- osa4/tests/note_api.test.js
+++ osa4/tests/note_api.test.js
@@ -4,11 +4,27 @@ const app = require('../app')
 
 const api = supertest(app)
 
+// beforeAll(() => {
+//   console.log('Alustus...')
+// })
+
 test('notes are returned as json', async () => {
   await api
     .get('/api/notes')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('there are nine notes', async () => {
+  const response = await api.get('/api/notes')
+
+  expect(response.body.length).toBe(9)
+})
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/notes')
+
+  expect(response.body[0].content).toBe('HTML on tylsää :(')
 })
 
 afterAll(() => {
