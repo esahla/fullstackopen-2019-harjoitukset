92bed712fe56bfb333202aac1032b47ae7754d2b
Esa Sahla <esa.sahla@nokia.com>
2019-11-14 17:33:33 +0200

    Osa8, harkkoja mongodbn kanssa

--- osa8/persons/index.js
+++ osa8/persons/index.js
@@ -1,28 +1,47 @@
 const { ApolloServer, gql, UserInputError } = require('apollo-server')
 const uuid = require('uuid/v1')
+const mongoose = require('mongoose')
+const Person = require('./models/person')
+const config = require('./utils/config')
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    phone: "040-123543",
-    street: "Tapiolankatu 5 A",
-    city: "Espoo",
-    id: "3d594650-3436-11e9-bc57-8b80ba54c431"
-  },
-  {
-    name: "Matti Luukkainen",
-    phone: "040-432342",
-    street: "Malminkaari 10 A",
-    city: "Helsinki",
-    id: '3d599470-3436-11e9-bc57-8b80ba54c431'
-  },
-  {
-    name: "Venla Ruuska",
-    street: "Nallemäentie 22 C",
-    city: "Helsinki",
-    id: '3d599471-3436-11e9-bc57-8b80ba54c431'
-  },
-]
+mongoose.set('useFindAndModify', false)
+mongoose.set('useUnifiedTopology', true)
+mongoose.set('useCreateIndex', true)
+
+
+console.log('Connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('\x1b[32m%s\x1b[0m', `Connected to MongoDB ${config.MONGODB_URI}`)
+  })
+  .catch((error) => {
+    console.log('\x1b[31m%s\x1b[0m', `Error connection to MongoDB: ${error.message}`)
+  })
+
+
+// let persons = [
+//   {
+//     name: "Arto Hellas",
+//     phone: "040-123543",
+//     street: "Tapiolankatu 5 A",
+//     city: "Espoo",
+//     id: "3d594650-3436-11e9-bc57-8b80ba54c431"
+//   },
+//   {
+//     name: "Matti Luukkainen",
+//     phone: "040-432342",
+//     street: "Malminkaari 10 A",
+//     city: "Helsinki",
+//     id: '3d599470-3436-11e9-bc57-8b80ba54c431'
+//   },
+//   {
+//     name: "Venla Ruuska",
+//     street: "Nallemäentie 22 C",
+//     city: "Helsinki",
+//     id: '3d599471-3436-11e9-bc57-8b80ba54c431'
+//   },
+// ]
 
 const typeDefs = gql`
   enum YesNo {
@@ -64,17 +83,14 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    personCount: () => persons.length,
+    personCount: () => Person.collection.countDocuments(),
     allPersons: (root, args) => {
       if (!args.phone) {
-        return persons
+        return Person.find({})
       }
-      const byPhone = (person) =>
-        args.phone === 'YES' ? person.phone : !person.phone
-      return persons.filter(byPhone)
+      return Person.find({ phone: { $exists: args.phone === 'YES'  }})
     },
-    findPerson: (root, args) =>
-      persons.find(p => p.name === args.name)
+    findPerson: (root, args) => Person.findOne({ name: args.name })
   },
   Person: {
     address: (root) => {
@@ -86,24 +102,28 @@ const resolvers = {
   },
   Mutation: {
     addPerson: (root, args) => {
-      if (persons.find(p => p.name === args.name)) {
-        throw new UserInputError('Name must be unique', {
-          invalidArgs: args.name,
-        })
-      }
-      const person = { ...args, id: uuid() }
-      persons = persons.concat(person)
-      return person
+      const person = new Person({ ...args })
+      return person.save()
+      // if (persons.find(p => p.name === args.name)) {
+      //   throw new UserInputError('Name must be unique', {
+      //     invalidArgs: args.name,
+      //   })
+      // }
+      // const person = { ...args, id: uuid() }
+      // persons = persons.concat(person)
+      // return person
     },
-    editNumber: (root, args) => {
-      const person = persons.find(p => p.name === args.name)
-      if (!person) {
-        return null
-      }
-  
-      const updatedPerson = { ...person, phone: args.phone }
-      persons = persons.map(p => p.name === args.name ? updatedPerson : p)
-      return updatedPerson
+    editNumber: async (root, args) => {
+      // const person = persons.find(p => p.name === args.name)
+      // if (!person) {
+      //   return null
+      // }
+      // const updatedPerson = { ...person, phone: args.phone }
+      // persons = persons.map(p => p.name === args.name ? updatedPerson : p)
+      // return updatedPerson
+      const person = await Person.findOne({ name: args.name })
+      person.phone = args.phone
+      return person.save()
     } 
   },
 }
